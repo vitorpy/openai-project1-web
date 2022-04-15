28649191129a1bd95f74e858f1855002e05f06b6
Vitor Py Braga <12871+vitorpy@users.noreply.github.com>
2022-04-15 20:20:51 +0100

    New feature: new fine tunes.

--- app.py
+++ app.py
@@ -43,7 +43,6 @@ def models():
     headers = {'Authorization': f'Bearer {openai.api_key}'}
 
     r = requests.get(url, headers=headers)
-    app.logger.info(f"*** {r}")
 
     if r.status_code != 200:
         return jsonify({"models": []})
@@ -51,10 +50,39 @@ def models():
     r = r.json() 
     model_names = []
     for element in r["data"]:
-        name = element["fine_tuned_model"]
-        model_names.append(name)
+        try:
+            name = element["fine_tuned_model"]
+        except Exception as e:
+            app.logger.info(f"*** Model unavailable: {e}")
+        else:
+            model_names.append(name)
     
     return jsonify({"models": model_names})
+
+
+@app.route("/_new_fine_tune", methods=['POST'])
+def new_fine_tune():
+    file_url = "https://api.openai.com/v1/files"
+    headers = {'Authorization': f'Bearer {openai.api_key}'}
+    
+    data = { "file": request.form["training-file"], "purporse": "fine-tune" }
+
+    r = requests.post(file_url, data=jsonify(data), headers=headers)
+    if r.status_code != 200:
+        return jsonify({"result": f"Failed to upload file to OpenAI {r}."})
+    r = r.json()
+
+    file_id = r["id"]
+
+    url = "https://api.openai.com/v1/fine-tunes"
+
+    data = { "training_file": file_id, "model": request.form["model"], "suffix": request.form["suffix"] }
+
+    r = requests.post(url, data=jsonify(data), headers=headers)
+    if r.status_code != 200:
+        return jsonify({"result": f"Failed to create fine-tuned model {r}."})
+
+    return jsonify({"result": "Model enqueued for training. It will be available soon (minutes)."})
 
 
 def generate_prompt(prompt):
